9c41ee3ef95f49e52cf866a99bcec102098019dd
YeniAlvarado <u20201b032@upc.edu.pe>
2023-05-24 16:18:38 -0500

    Flutter App

---
+++ android/app/src/main/kotlin/com/example/app_sem10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_sem10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FirstRoute(),
+  ));
+}
+class FirstRoute extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text(" Ir a sgte ventana "),
+          onPressed: (){
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context)=> SecondRoute())
+            );
+          },
+        ),
+      ),
+    );
+
+  }
+}
+class SecondRoute extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text(" Regresar"),
+          onPressed: (){
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context)=> FirstRoute())
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
